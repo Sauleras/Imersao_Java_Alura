1ed36c9bfebf079a8591b8228add714cd098ed0f
Sauleras <saulojuniosantana@gmail.com>
2023-03-29 10:28:22 -0300

    Imersão Java: Segundo dia

--- src/AluraStickers.java
+++ src/AluraStickers.java
@@ -1,5 +1,7 @@
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -8,7 +10,7 @@ import java.util.List;
 import java.util.Map;
 
 public class AluraStickers {
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws Exception {
 
         //fazer uma conexão HTTP e buscar os top 250 filmes
         String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
@@ -24,13 +26,19 @@ public class AluraStickers {
         List<Map<String, String>> listaDeFilmes = parser.parse(body);
 
         //exibir e manipular os dados
+        var geradora = new GeradoraDeFigurinhas();
         for(Map<String,String> filme : listaDeFilmes) {
-
-            System.out.println("Posição: "+ filme.get("rank"));
-            System.out.println("Título: "+filme.get("title"));
-            System.out.println("Classificação: "+filme.get("imDbRating"));
-
             var countEstrela = Float.parseFloat(filme.get("imDbRating"));
+            String urlImagem = filme.get("image");
+            String titulo = filme.get("title");
+
+            InputStream inputStream = new URL(urlImagem).openStream();
+            String nomeArquivo = titulo + ".png";
+
+            geradora.cria(inputStream, nomeArquivo);
+
+            System.out.println("Título: "+titulo);
+            System.out.println("Classificação: "+filme.get("imDbRating"));
 
             for(int x = 0; x < Math.floor(countEstrela); x++) {
                 System.out.print("\u2B50");
